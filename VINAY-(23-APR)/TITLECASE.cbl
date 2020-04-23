8440e7696362d15304fc786b1873bcdf7a41f9a2
VINAY1445 <64063230+VINAY1445@users.noreply.github.com>
2020-04-23 11:25:34 +0530

    Add files via upload

---
+++ VINAY-(23-APR)/TITLECASE.cbl
@@ -0,0 +1,33 @@
+ 
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TITLECASE.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       01 WS-FSEN PIC X(100).
+       01 WS-SEN  PIC X(100).
+       01 WS-TITLE PIC X(100).
+       01 WS-LEN PIC 999.
+       01 WS-I PIC 999.
+       01 WS-CURR PIC X.
+       01 WS-PREV PIC X.
+       01 WS-NEXT PIC X.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           ACCEPT WS-FSEN
+           MOVE FUNCTION LOWER-CASE(WS-FSEN) TO WS-SEN
+           MOVE LENGTH OF WS-FSEN TO WS-LEN
+           MOVE WS-SEN(1:1) TO WS-PREV
+           PERFORM VARYING WS-I FROM 2 BY 1 UNTIL WS-I > WS-LEN
+              MOVE FUNCTION UPPER-CASE(WS-SEN(1:1)) TO WS-TITLE(1:1)
+              MOVE WS-SEN(WS-I:1) TO WS-CURR
+              IF ( WS-PREV = ' ') THEN
+                  MOVE FUNCTION UPPER-CASE( WS-CURR) TO WS-TITLE(WS-I:1)
+              ELSE
+                  MOVE WS-CURR TO WS-TITLE(WS-I:1)
+              END-IF
+              MOVE WS-CURR TO WS-PREV
+           END-PERFORM
+            DISPLAY WS-TITLE
+            STOP RUN.
+       END PROGRAM TITLECASE.
